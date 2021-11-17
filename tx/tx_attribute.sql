b152502505ad668cb9c1a121d98fdad0c5cf44e6
neptuneresearch <neptuneresearch@protonmail.com>
2021-11-17 03:23:45 +0000

    tx_attribute: add material form

--- tx/tx_attribute.sql
+++ tx/tx_attribute.sql
@@ -13,3 +13,26 @@ CREATE VIEW tx_attribute AS
     FROM monero block,
     LATERAL unnest(block.transactions) WITH ORDINALITY tx(hash, version, unlock_time, vin, vout, extra, fee)
     ORDER BY block.height ASC, tx.ordinality ASC;
+
+-- This is a view based on an unindexed array column (monero.transactions), so JOINing it on anything
+-- besides block height will be slow. For JOIN usage, materialize it up to a certain height,
+-- and index tx_hash.
+CREATE MATERIALIZED VIEW tx_attribute_2489999 AS (
+    SELECT 
+        block."height" AS block_height,
+        block."timestamp" AS block_timestamp,
+        tx.ordinality AS tx_index,
+        tx.hash AS tx_hash,
+        tx."version" AS tx_version,
+        tx.unlock_time AS tx_unlock_time,
+        CARDINALITY(tx.vin) AS tx_n_inputs,
+        CARDINALITY(tx.vout) AS tx_n_outputs,
+        OCTET_LENGTH(tx.extra) AS tx_len_extra,
+        tx.fee AS tx_fee
+    FROM monero block,
+    LATERAL unnest(block.transactions) WITH ORDINALITY tx(hash, version, unlock_time, vin, vout, extra, fee)
+    WHERE block.height <= 2489999
+    ORDER BY block.height ASC, tx.ordinality ASC
+) WITH NO DATA;
+
+CREATE INDEX tx_attribute_2489999_tx_hash ON tx_attribute_2489999 (tx_hash);
